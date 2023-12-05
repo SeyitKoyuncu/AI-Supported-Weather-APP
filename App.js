f5213842c12764a88edfa2a6f0add1e2261d09ff
Seyit KOYUNCU <98276834+SeyitKoyuncu@users.noreply.github.com>
2023-12-05 13:52:28 +0300

    starting of project

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log("Hello World")
+  return (
+    <View style ={{ height: 100, width: 100, backgroundColor: 'orange'}}>
+      <Text> Hello World </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
